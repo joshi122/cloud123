44063953d609577839a5ec01f9abcbd301cf0193
sudarshan joshi <joshisudarshan068@gmail.com>
2025-02-18 00:09:02 +0530

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -1,6 +1,10 @@
 resource "aws_efs_file_system" "terr_efs" {
   creation_token = "my-product"
 
+lifecycle {
+    ignore_changes = [creation_token]
+  }
+
   encrypted = true
   tags = {
     Name = "efs-terr"
